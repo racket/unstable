21793f59f47922b384809f712204f8abea321e8a
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-16 16:13:25 -0500

    Move most of unstable/list to racket/list.
    See corresponding commit in unstable-list-lib.

--- unstable-test/tests/unstable/list.rkt
+++ unstable-test/tests/unstable/list.rkt
@@ -1,11 +1,5 @@
 #lang scheme
 (require unstable/list)
-(require tests/eli-tester)
-(test
- (remf positive? '()) => '()
- (remf positive? '(1 -2 3 4 -5)) => '(-2 3 4 -5)
- (remf even? '(1 -2 3 4 -5)) => '(1 3 4 -5)
- (remf (λ (x) #f) '(1 -2 3 4 -5)) => '(1 -2 3 4 -5))
 
 (require rackunit rackunit/text-ui)
 
@@ -37,27 +31,4 @@
                       (map/values 3 values '(1 2 3) '(4 5 6) '(7 8 9))])
          (check-equal? as '(1 2 3))
          (check-equal? bs '(4 5 6))
-         (check-equal? cs '(7 8 9)))))
-   (test-suite "group-by"
-       (check-equal? (group-by values '(1 4 2 56 2 3))
-                     '((1) (4) (2 2) (56) (3)))
-       (check-equal? (group-by values '(1 2 1 2 54 2 5 43 7 2 643 1 2 0))
-                     '((1 1 1) (2 2 2 2 2) (54) (5) (43) (7) (643) (0)))
-       (check-equal? (group-by values
-                               '(1 4 2 56 2 3)
-                               (lambda (x y) (or (and (even? x) (even? y))
-                                                 (and (odd?  x) (odd?  y)))))
-                     '((1 3) (4 2 56 2)))
-       (check-equal? (group-by car '((1 a) (4 b) (2 c) (56 d) (2 e) (3 f)))
-                     '(((1 a)) ((4 b)) ((2 c) (2 e)) ((56 d)) ((3 f))))
-       (check-equal? (group-by even? '(1 2 3 4 5 6))
-                     '((1 3 5) (2 4 6))))
-   (test-suite "cartesian-product"
-     (check-equal? (cartesian-product '(1 2 3) '(a b c))
-                   '((1 a) (1 b) (1 c)
-                     (2 a) (2 b) (2 c)
-                     (3 a) (3 b) (3 c)))
-     (check-equal? (cartesian-product '(4 5 6) '(d e f) '(#t #f))
-                   '((4 d #t) (4 d #f) (4 e #t) (4 e #f) (4 f #t) (4 f #f)
-                     (5 d #t) (5 d #f) (5 e #t) (5 e #f) (5 f #t) (5 f #f)
-                     (6 d #t) (6 d #f) (6 e #t) (6 e #f) (6 f #t) (6 f #f))))))
+         (check-equal? cs '(7 8 9)))))))
