4dfe2bc6fde17477adc635004319d3a9ba5e1d84
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-31 16:53:54 -0500

    Move tests for syntax-source-* to syntax tests.

--- unstable-test/tests/unstable/syntax.rkt
+++ unstable-test/tests/unstable/syntax.rkt
@@ -1,40 +1,14 @@
 #lang racket
 
-(require mzlib/etc
-         rackunit
+(require rackunit
          rackunit/text-ui
          racket/syntax
          (for-syntax unstable/syntax)
          unstable/syntax
          "helpers.rkt")
 
-(define here
-  (datum->syntax
-   #f 'here
-   (list (build-path (this-expression-source-directory)
-                     (this-expression-file-name))
-         1 1 1 1)))
-
 (run-tests
  (test-suite "syntax.rkt"
-
-   (test-suite "Syntax Source Locations"
-
-     (test-suite "syntax-source-file-name"
-       (test-case "here"
-         (check-equal? (syntax-source-file-name here)
-                       (this-expression-file-name)))
-       (test-case "fail"
-         (check-equal? (syntax-source-file-name (datum->syntax #f 'fail))
-                       #f)))
-
-     (test-suite "syntax-source-directory"
-       (test-case "here"
-         (check-equal? (syntax-source-directory here)
-                       (this-expression-source-directory)))
-       (test-case "fail"
-         (check-equal? (syntax-source-directory (datum->syntax #f 'fail))
-                       #f))))
 
    (test-suite "Pattern Bindings"
 
