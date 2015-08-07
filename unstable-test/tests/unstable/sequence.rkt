9f0f9d0f55bb9bd758d78004cdc150ceaeaf3282
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-07 15:41:02 -0500

    Move tests for the moved portions of `unstable/sequence` to the core tests.

--- unstable-test/tests/unstable/sequence.rkt
+++ unstable-test/tests/unstable/sequence.rkt
@@ -14,16 +14,4 @@
   (check-equal? (for/list ([x (in-sequence-forever (in-range 5) 5)]
                            [y (in-range 10)])
                   x)
-                '(0 1 2 3 4 5 5 5 5 5))
-  (check-equal? (for/list ([(x y) (in-pairs '((1 . 1) (2 . 2) (3 . 3)))])
-                  (+ x y))
-                '(2 4 6))   
-  (check-true (sequence? (in-slice 1 '())))
-  (check-equal? '() (for/list ([v (in-slice 1 '())]) v))
-  (check-equal? '((0 1)) (for/list ([v (in-slice 3 (in-range 2))]) v))
-  (check-equal? '((0 1 2) (3 4 5))
-                (for/list ([v (in-slice 3 (in-range 6))]) v))
-  (check-equal? '((0 1 2) (3 4 5) (6 7))
-                (for/list ([v (in-slice 3 (in-range 8))]) v))
-  (check-exn exn:fail:contract?
-             (lambda () (for/list ([x (in-slice 0 (in-range 8))]) x)))))
+                '(0 1 2 3 4 5 5 5 5 5))))
