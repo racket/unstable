d43ef04b6f5a245818ed533ccfb3f04dce4b33ab
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-21 15:19:00 -0400

    Move color utilities to pict/color.

--- unstable-lib/gui/pict.rkt
+++ unstable-lib/gui/pict.rkt
@@ -28,44 +28,19 @@
 
 (define (color c p) (colorize p c))
 
-(define color/c
-  (or/c string? ;; might be faster
-        ;;(and/c string? (lambda (s) (send the-color-database find-color s)))
-        (is-a?/c color%)
-        (list/c byte? byte? byte?)))
-
-(define-syntax-rule (define-colors name ...)
-  (begin (define (name pict) (color (symbol->string 'name) pict)) ...))
-
-(define-colors
-  red orange yellow green blue purple
-  black brown gray white cyan magenta)
-
-(define (light c) (scale-color 2 c))
-(define (dark c) (scale-color 1/2 c))
-
 (provide/contract
- [color/c flat-contract?]
- [red     (-> pict? pict?)]
- [orange  (-> pict? pict?)]
- [yellow  (-> pict? pict?)]
- [green   (-> pict? pict?)]
- [blue    (-> pict? pict?)]
- [purple  (-> pict? pict?)]
- [black   (-> pict? pict?)]
- [brown   (-> pict? pict?)]
- [gray    (-> pict? pict?)]
- [white   (-> pict? pict?)]
- [cyan    (-> pict? pict?)]
- [magenta (-> pict? pict?)]
- [light (-> color/c color/c)]
- [dark (-> color/c color/c)]
  [color (-> color/c pict? pict?)]
  [fill
   (-> pict?
       (or/c (real-in 0 +inf.0) #f)
       (or/c (real-in 0 +inf.0) #f)
       pict?)])
+
+(require pict/color) ; for re-export
+(provide color/c
+         red orange yellow green blue purple
+         black brown gray white cyan magenta
+         light dark)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
