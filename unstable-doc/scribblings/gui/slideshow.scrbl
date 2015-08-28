a08a705e88ff0b0f926bdf9ebdc48f2122093fcb
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-28 17:02:08 -0500

    Move text formatting utilities to slideshow/text.

--- unstable-doc/scribblings/gui/slideshow.scrbl
+++ unstable-doc/scribblings/gui/slideshow.scrbl
@@ -5,63 +5,16 @@
                      unstable/gui/slideshow))
 
 @title{Slideshow Presentations}
-@unstable[@author+email["Carl Eastlund" "cce@racket-lang.org"]]
+
+@deprecated[@racketmodname[slideshow]]{
+The contents of this module, with the exceptions below, have been merged with
+the @racketmodname[slideshow] collection, or its subcollections.
+}
 
 @defmodule[unstable/gui/slideshow]
 
 This module also exports everything provided by
 @racketmodname[unstable/gui/pict].
-
-@section{Text Formatting}
-
-@defform[(with-size size expr)]{
-
-Sets @racket[current-font-size] to @racket[size] while running @racket[expr].
-
-}
-
-@defform[(with-scale scale expr)]{
-
-Multiplies @racket[current-font-size] by @racket[scale] while running
-@racket[expr].
-
-}
-
-@deftogether[(
-@defform[(big text)]
-@defform[(small text)]
-)]{
-
-Scale @racket[current-font-size] by @racket[3/2] or @racket[2/3], respectively,
-while running @racket[text].
-
-}
-
-@defform[(with-font font expr)]{
-
-Sets @racket[current-main-font] to @racket[font] while running @racket[expr].
-
-}
-
-@defform[(with-style style expr)]{
-
-Adds @racket[style] to @racket[current-main-font] (via @racket[cons]) while
-running @racket[expr].
-
-}
-
-@deftogether[(
-@defform[(bold text)]
-@defform[(italic text)]
-@defform[(subscript text)]
-@defform[(superscript text)]
-@defform[(caps text)]
-)]{
-
-Adds the attributes for bold, italic, superscript, subscript, or small caps
-text, respectively, to @racket[current-main-font] while running @racket[text].
-
-}
 
 @section{Tables}
 
@@ -205,12 +158,4 @@ font so as to indicate inactivity.
     @t{I will show up for item3}))
 ]
 
-}
-
-@section{Miscellaneous Slide Utilities}
-
-@addition{Scott Owens}
-
-@defproc[(blank-line) pict?]{
-Adds a blank line of the current font size's height.
 }
--- unstable-lib/gui/slideshow.rkt
+++ unstable-lib/gui/slideshow.rkt
@@ -1,58 +1,22 @@
 #lang racket/base
 (require slideshow/base pict
+         slideshow/text ; for re-export
          racket/contract/base racket/list racket/match
          racket/stxparam
          (for-syntax racket/base racket/list racket/set syntax/parse)
          "pict.rkt")
-(provide (all-from-out "pict.rkt"))
-
-(define-syntax-rule (define-with-parameter name parameter)
-  (define-syntax-rule (name value body (... ...))
-    (parameterize ([parameter value]) body (... ...))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;  Font Controls
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define-with-parameter with-size current-font-size)
-(define-syntax-rule (with-scale scale expr)
-  (with-size (inexact->exact (ceiling (* scale (current-font-size)))) expr))
-(define-syntax-rule (define-scale name scale)
-  (define-syntax-rule (name expr) (with-scale scale expr)))
-(define-scale big 3/2)
-(define-scale small 2/3)
-
-(define-with-parameter with-font current-main-font)
-(define-syntax-rule (with-style style expr)
-  (with-font (cons style (current-main-font)) expr))
-(define-syntax-rule (define-style name style)
-  (define-syntax-rule (name expr) (with-style style expr)))
-(define-style bold 'bold)
-(define-style italic 'italic)
-(define-style subscript 'subscript)
-(define-style superscript 'superscript)
-(define-style caps 'caps)
-
-(provide with-size
-         with-scale
-         big
-         small
-
-         with-font
-         with-style
-         bold
-         italic
-         subscript
-         superscript
-         caps)
+(provide (all-from-out "pict.rkt")
+         (all-from-out slideshow/text))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;  Slide / Paragraph Manipulation
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-syntax-rule (define-with-parameter name parameter)
+  (define-syntax-rule (name value body (... ...))
+    (parameterize ([parameter value]) body (... ...))))
 
 (define-with-parameter column current-para-width)
 
@@ -151,12 +115,6 @@
 ;;  Misc
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (blank-line)
-  (blank 0 (current-font-size)))
-
-(provide/contract
- [blank-line (-> pict?)])
 
 ;; Revealing slides. Similar to staging slides, can probably be implemented
 ;; in terms of staged slides.
